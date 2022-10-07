0184213b9655f9fd4e527ae21fcdf67a21ba2f49
unknown <git config --global user.email>
2022-10-07 18:10:38 +0100

    Creating the game

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,27 +1,8 @@
-import com.googlecode.lanterna.TextCharacter;
-import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.screen.TerminalScreen;
-import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import com.googlecode.lanterna.terminal.Terminal;
-
 import java.io.IOException;
 
 public class Application {
-    public static void main(String[] args) {
-        try {
-            Terminal terminal = new
-                    DefaultTerminalFactory().createTerminal();
-            Screen screen = new TerminalScreen(terminal);
-            screen.setCursorPosition(null);
-            screen.startScreen();
-            screen.doResizeIfNecessary();
-            screen.clear();
-            screen.setCharacter(10, 10, TextCharacter.fromCharacter('X')
-                    [0]);
-            screen.refresh();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException {
+        Game game = new Game();
+        game.run();
     }
 }
